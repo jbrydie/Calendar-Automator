5b41e159437d91ee5cb4446da9b44acf5364d08e
jbrydie <jordan.brydie@ey.com>
2019-05-11 15:17:26 -0400

    start building chart

---
+++ piechart.py
@@ -0,0 +1,7 @@
+import plotly.plotly as plt
+import plotly.graph_objs as go
+
+labels = ['Work', 'Travel', 'Gym', 'Dinner Reservations', 'Code']
+sizes = [] #this needs to take input from my google calendar, should work dynamically
+
+
